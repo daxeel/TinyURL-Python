23cfc0517940c46166bd23a839fdea900874b637
Daxeel Soni <daxeel_soni4444@yahoo.com>
2016-01-06 22:45:11 +0530

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='tinyurl',
+      version='1.0',
+      description='Python module to create short links from tinyurl.com',
+      author='Daxeel Soni',
+      author_email='sayhi@daxeelsoni.in',
+      url='http://www.daxeelsoni.in',
+      keywords = ['url', 'shortner', 'tinyurl'],
+      py_modules=['tinyurl'],
+     )
